47b9c8db60a9a2f41ca2d9bad350946d55037829
Gautham Bhandary <gauthambhandary05@gmail.com>
2024-03-26 09:33:11 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is part of a database that holds only one type of information?",
+    answer: "Field",
+    options: [
+      "Report",
+      "Record",
+      "Field",
+      "File"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the full form of DBMS?",
+    answer: "Database Management System",
+    options: [
+      "Data of Binary Management System",
+      "Database Management Service",
+      "Database Management System",
+      "Data Backup Management System"
+    ]
+  },
+    {
+    numb: 3,
+    question: "'.MOV' extension refers usually to what kind of file?",
+    answer: "Animation/movie file",
+    options: [
+      "Image file",
+      "Animation/movie file",
+      "Audio file",
+      "MS office document"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which data structure is used for implementing recursion?",
+    answer: "Stack",
+    options: [
+      "Queue",
+      "Stack",
+      "List",
+      "Array"
+    ]
+  },
+    {
+    numb: 5,
+    question: "CPU scheduling is the basis of",
+    answer: "multiprogramming operating systems",
+    options: [
+      "larger memory sized systems",
+      "multiprocessor systems",
+      "multiprogramming operating systems",
+      "none of the mentioned"
+    ]
+  },
+];
